3bdf613837c56c2408fa6682bc5738bd314f9edc
thainafonsecadev <thainafonsecac.m@outlook.com>
2025-07-01 22:55:56 -0300

    operacao de instanciacao da base de dados

--- src/main/java/com/thainafonsecadev/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/thainafonsecadev/workshopmongo/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.thainafonsecadev.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,10 @@ import com.thainafonsecadev.workshopmongo.domain.User;
 
 	@Repository
 	public interface UserRepository extends MongoRepository<User, String> {
+
+		static void save(List<User> asList) {
+			
+		}
 	
 
 }
