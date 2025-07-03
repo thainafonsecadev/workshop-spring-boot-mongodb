dcb25a1af872c1a7df4e0b9b3ac4e5048fac5085
thainafonsecadev <thainafonsecac.m@outlook.com>
2025-07-02 22:59:04 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/thainafonsecadev/workshopmongo/resource/UserResource.java
+++ src/main/java/com/thainafonsecadev/workshopmongo/resource/UserResource.java
@@ -6,12 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.thainafonsecadev.workshopmongo.domain.User;
@@ -48,6 +43,12 @@ import com.thainafonsecadev.workshopmongo.service.UserService;
 		URI uri =ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 		
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 }	
